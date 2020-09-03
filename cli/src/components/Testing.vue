c33497c622470d3353887dbe0a98750a954afbe3
YinWaiAungMIC <yinwaiaungcsf1@gmail.com>
2020-09-03 14:28:17 +0630

    Vue Updated

---
+++ cli/src/components/Testing.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>{{title}}</h1>
+		<p>{{msg}}</p>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		props:{
+			msg:null
+		},
+		data (){
+			return {
+				title: 'Testing Vue'
+
+			}
+		}
+	}
+	
+</script>
