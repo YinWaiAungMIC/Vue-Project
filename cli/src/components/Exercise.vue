abdf3401eef513ed375b81a3aae6c76be124a0ac
YinWaiAungMIC <yinwaiaungcsf1@gmail.com>
2020-09-04 13:38:41 +0630

    Vue Updated

---
+++ cli/src/components/Exercise.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<h1 class="text-danger">Exercise Component</h1>
+		<p>This is exercise for child components.</p>
+	</div>
+</template>
+<script type="text/javascript">
+	
+</script>
